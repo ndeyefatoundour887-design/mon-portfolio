8f56b2817607aa7dfd04034e7809589b28c787a0
ndeyefatoundour887-design <ndeyefatoundour887@gmail.com>
2026-03-27 16:37:46 +0100

    Miss Ndour

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,27 @@
-export default function Contact() { 
-  // ... ton code
+function Contact() {
+  return (
+    <section className="contact-container">
+
+      <h2>Contactez-moi</h2>
+
+      <p>Vous pouvez me contacter en utilisant le formulaire ci-dessous :</p>
+      <div className="contact-info">
+        <p>📞 +221 77 961 57 01</p>
+        <p>📧 ndeyefatoundour887@gmail.com</p>
+        <p>📍 Thiès, Sénégal</p>
+      </div>
+
+      <form className="contact-form">
+        <input type="text" placeholder="Nom" />
+        <input type="text" placeholder="Prénom" />
+        <input type="email" placeholder="Email" />
+        <input type="tel" placeholder="Téléphone" />
+
+        <button type="submit">Envoyer</button>
+      </form>
+
+    </section>
+  )
 }
+
+export default Contact
